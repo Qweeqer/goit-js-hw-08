796e26fc00f5bb40eb42e8ef27ab8cdee0c89edf
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-04 16:59:26 +0300

    srat task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+  
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", `${data.seconds}`)
+    console.log(localStorage)
+};
+
+player.on('timeupdate',throttle(onPlay, 1000));
+
+const videoPlayerTime = localStorage.getItem("videoplayer-current-time"); 
+player.setCurrentTime(videoPlayerTime)

d83e7804344e38057363f74dc459ae7b87b37d2f
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-04 20:11:05 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,9 +10,7 @@ if (feedBackRef !== null) {
     const { email, message } = JSON.parse(feedBackRef);
     form.email.value = email ? email : '';
     form.message.value = message ? message : '';
-} if (feedBackRef === null) {
-    return alert("Заполни все поля");
-}
+} 
 
 email.addEventListener('input', throttle(handleInput,500))
 message.addEventListener('input', throttle(handleInput,500))
@@ -23,25 +21,11 @@ function handleInput({target}) {
     localStorage.setItem("feedback-form-state", JSON.stringify(state))
 }
 
-function clearSubmitForm(e){
-    e.preventDefault()
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')))  
+function clearSubmitForm(event){
+    event.preventDefault()
+    console.log(JSON.parse(feedBackRef))  
     localStorage.removeItem('feedback-form-state')
     form.email.value = "";
     form.message.value = "";
 }
-// const LOCALSTORAGE_KEY = "goit-example-message";
 
-// updateOutput();
-// form.addEventListener("submit", saveMessage);
-
-// function saveMessage(evt) {
-//   evt.preventDefault();
-//   localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
-//   updateOutput();
-//   form.reset();
-// }
-
-// function updateOutput() {
-//   output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
-// }

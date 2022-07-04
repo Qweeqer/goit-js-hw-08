9accdcbb1078634d3155264c6138fe0c68b68b6d
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-04 17:35:21 +0300

    update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,15 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
   
-const onPlay = function (data) {
-    localStorage.setItem("videoplayer-current-time", `${data.seconds}`)
-    console.log(localStorage)
+const onPlay = function (time) {
+    localStorage.setItem("videoplayer-current-time", `${time.seconds}`);
+    console.log(localStorage);
 };
+player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
 
 player.on('timeupdate',throttle(onPlay, 1000));
 
 const videoPlayerTime = localStorage.getItem("videoplayer-current-time"); 
-player.setCurrentTime(videoPlayerTime)
+player.setCurrentTime(videoPlayerTime);

230b441a27fd6de6ff62425878588340dd8331d5
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2022-07-06 18:26:27 +0300

    task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,44 +3,57 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector(".feedback-form");
 const email = document.querySelector(".feedback-form input");
 const message = document.querySelector(".feedback-form textarea");
+const userData = document.querySelectorAll("input");
 const feedBackRef = localStorage.getItem('feedback-form-state');
-const state = feedBackRef ? JSON.parse(feedBackRef) : {};
+const formData = {};
+const state = localStorage.getItem('feedback-form-state')
+    ? JSON.parse(feedBackRef) : formData;
 
 if (feedBackRef !== null) {
     const { email, message } = JSON.parse(feedBackRef);
     form.email.value = email ? email : '';
     form.message.value = message ? message : '';
-} 
-email.addEventListener('input', throttle(handleInput, 500));
-message.addEventListener('input', throttle(handleInput, 500));
-form.addEventListener('submit', clearSubmitForm);
+}
 
-function handleInput({target}) {
+
+form.addEventListener('submit', clearSubmitForm);
+form.addEventListener('input', throttle(handleInput, 500));
+
+function handleInput({ target }) {
+
     state[target.name] = target.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(state));
 }
 
-function clearSubmitForm(event){
-    event.preventDefault();
-    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));  
-    localStorage.removeItem('feedback-form-state');
-    form.email.value = "";
-    form.message.value = "";
+function clearSubmitForm(event) {
+    if (form.email.value === "" || form.message.value === "") {
+        alert(`Warning! All fields must be filled!`);
+        event.currentTarget.reset();
+    } else {   
+        event.preventDefault();
+        console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+        localStorage.removeItem('feedback-form-state');
+        form.email.value = "";
+        form.message.value = "";
+    }
 }
+
 // <--------------------Чернетка---->
+
 // <-------------Перевірка вводу---------------------------->
 // form.addEventListener("submit", handleSubmit);
 
 // function handleSubmit(event) {
 //     event.preventDefault();
 //     const {
-//         elements: { email, password}
+//         elements: { email, message}
 //     } = event.currentTarget;
 //     if (form.email.value === "" || form.message.value === "") {
-//         return alert(`Warning! All fields must be filled!`);
-//     } 
-//     console.log("Email:", form.email.value, "Password:", form.message.value);
-//     event.currentTarget.reset();
+//         return alert(`Warning! All fields must be filled!`);        
+//     } else {
+//         console.log("Email:", form.email.value, "Message:", form.message.value);
+//         event.currentTarget.reset();
+//     }
 // };
 // <----------------------------------------------------->
 
